b37104345be16019522f7df3d2e9d8eb626cdee2
Fırat Yılmaz <yilmaz8596@gmail.com>
2025-01-05 19:44:18 +0300

    ilgili alt projelere ait sınıf dosyaları oluşturuldu

---
+++ YemekSiparişProjesi-KatmanlıMimari.Business/Validators/UserValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YemekSiparişProjesi_KatmanlıMimari.Business.Validators
+{
+    internal class UserValidator
+    {
+    }
+}
